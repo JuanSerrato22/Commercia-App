76086c56b1adf7356cf4bc0ba2f374ff0c1ad7ff
JuanSerrato22 <serratojuan228@gmail.com>
2025-07-09 21:46:31 -0500

    Cambios del proyecto

--- src/api/contants/Endpoint.ts
+++ src/api/contants/Endpoint.ts
@@ -1,8 +1,8 @@
-const API_BASE_URL="http://172.30.1.79:8080/api/v1";
+const API_BASE_URL = "http://172.30.1.79:8085/api/v1";
 
-export const PRODUCT_END_POINT= `${API_BASE_URL}/product/`; 
-export const CATEGORY_END_POINT = `${API_BASE_URL}categories/`;
-export const SUPPLIER_END_POINT = `${API_BASE_URL}suppliers/`;
-export const CLIENT_END_POINT = `${API_BASE_URL}clients/`;
-export const ORDER_END_POINT = `${API_BASE_URL}orders/`;
-export const ORDER_ITEM_END_POINT = `${API_BASE_URL}order-items/`;
+export const PRODUCT_END_POINT   = `${API_BASE_URL}/product`;
+export const CATEGORY_END_POINT = `${API_BASE_URL}/categories`;
+export const SUPPLIER_END_POINT = `${API_BASE_URL}/suppliers`;
+export const CLIENT_END_POINT   = `${API_BASE_URL}/clients`;
+export const ORDER_END_POINT    = `${API_BASE_URL}/orders`;
+export const ORDER_ITEM_END_POINT = `${API_BASE_URL}/order-items`;
